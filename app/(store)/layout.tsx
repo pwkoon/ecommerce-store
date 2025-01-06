2f78184b9e2803351dd1131e63db394fea5940ca
wankoon <puahwankoom@gmail.com>
2025-01-06 16:23:55 +0800

    update

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider dynamic>
+      <html lang="en">
+        <body>
+          <main>
+            <Header />
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
