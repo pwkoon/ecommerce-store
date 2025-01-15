30ac719a89669896ebd461a99dcd9004839993e7
wankoon <puahwankoom@gmail.com>
2025-01-16 01:54:00 +0800

    update

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import { PortableText } from "next-sanity";
+import AddToBasketButton from "@/components/AddToBasketButton";
 
 async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
@@ -33,14 +34,19 @@ async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
           )}
         </div>
         <div className="flex flex-col justify-between">
-          <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
-          <div className="text-xl font-semibold mb-4">
-            ${product.price?.toFixed(2)}
+          <div>
+            <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
+            <div className="text-xl font-semibold mb-4">
+              ${product.price?.toFixed(2)}
+            </div>
+            <div className="prose max-w-none mb-6">
+              {Array.isArray(product.description) && (
+                <PortableText value={product.description} />
+              )}
+            </div>
           </div>
-          <div className="prose max-w-none mb-6">
-            {Array.isArray(product.description) && (
-              <PortableText value={product.description} />
-            )}
+          <div className="mt-6 ">
+            <AddToBasketButton product={product} disabled={isOutOfStock} />
           </div>
         </div>
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -10,11 +10,14 @@ import {
 import Link from "next/link";
 import Form from "next/form";
 import { PackageIcon, TrolleyIcon } from "@sanity/icons";
+import useBasketStore from "@/store";
+import { useEffect, useState } from "react";
 
 function Header() {
   const { user } = useUser();
-
-  console.log(user);
+  const itemCount = useBasketStore((state) =>
+    state.items.reduce((total, item) => total + item.quantity, 0)
+  );
 
   const createClerkPasskey = async () => {
     try {
@@ -52,6 +55,9 @@ function Header() {
           >
             <TrolleyIcon className="w-6 h-6" />
             {/* span item count once global state is implemented */}
+            <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+              {itemCount}
+            </span>
             <span>My Basket</span>
           </Link>
           {/* User area */}
